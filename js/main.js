d11b24a22b75d48bde5db723124cbbe50d49059d
Your Name <you@example.com>
2021-12-13 12:25:25 +0100

    js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let przepis = document.getElementById("#opis");
+
+console.log(przepis);
+
+let button = document.querySelector('#opis');
+
+console.log(button);
+
+button.onclick = function() {
+  alert('przepis działa!');
+}
